b9b56e25fdad69ec21031e07705af3869f9230ba
MinKhantKyawSwar <mkks1986.mkks@gmail.com>
2024-03-14 10:05:13 +0630

    First Commit

---
+++ src/components/List.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Card from './Card';
+const List = (props) => {
+  return (
+    <>
+      {
+        props.userInfos.length > 0 ? (<>
+        {props.userInfos.map((info)=> (
+          <Card css={"card-mt"} key = {info.email}>
+            <div className='list'>
+              <p>Name : {info.name}</p>
+              <p>From : {info.live}</p>
+              <p>Email : {info.email}</p>
+          </div>
+      </Card>
+      ))}
+        </>) : (
+        <>
+        <Card css={"card-mt"}>
+          <p>Add new user information!</p>
+        </Card>
+        </>)
+      }
+    </>
+  )
+}
+
+export default List
